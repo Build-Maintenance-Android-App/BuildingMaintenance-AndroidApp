75ea1719a680cc770ad462f6e761fe8739ae2e2b
erentrk01 <erentarak1907@gmail.com>
2022-12-29 10:35:50 +0300

    recylerview template

--- app/src/main/java/com/android/buildingmaintenanceapp/adapters/CustomRecyclerViewAdapter.java
+++ app/src/main/java/com/android/buildingmaintenanceapp/adapters/CustomRecyclerViewAdapter.java
@@ -1,4 +1,37 @@
 package com.android.buildingmaintenanceapp.adapters;
 
-public class CustomRecyclerViewAdapter {
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.buildingmaintenanceapp.models.Event;
+
+import java.util.ArrayList;
+
+public class CustomRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder >{
+    Context context;
+    private ArrayList<Event> recyclerItemValues;
+
+    public CustomRecyclerViewAdapter(Context context, ArrayList<Event> recyclerItemValues) {
+        this.context = context;
+        this.recyclerItemValues = recyclerItemValues;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
